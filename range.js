bfb19d8c5999acbb125a2db7eda1a06311832f8e
Hooman Asadian <hasadian@hotmail.com>
2023-03-19 20:20:48 -0700

    added if statement as requested by the assignment

--- range.js
+++ range.js
@@ -1,10 +1,18 @@
 const range = function (start, end, step){
   let myArr = [];
-  for (let i = start; i <= end; i += step){
-    myArr.push(i);
+  if (start > end || step <= 0 || start == undefined || end == undefined || step == undefined){
+    return myArr
+  } else {
+    for (let i = start; i <= end; i += step){
+      myArr.push(i);
+    }
   }
+  
   return myArr;
 }
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+console.log(range(1, 5, 0));
+console.log(range(8, 5, 0));
+console.log(range());
